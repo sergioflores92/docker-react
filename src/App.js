60e93231b767ce4a4bbafe0722681bd0e59f6abe
sergioflores92 <sergio.flores.burquez@gmail.com>
2021-07-09 17:56:41 -0600

    simple change in src code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mexicanos al grito de guerra.
+          Puto el que lo lea.
         </p>
         <a
           className="App-link"
